5f454e6f9a5d7ea9d5cd7ac1791d2e68817f3b68
Krishna Chaitanya Balasa <krishnachaitanyabalasa@gmail.com>
2026-06-13 23:27:16 +0530

    first commit

--- src/main/java/com/chaitu/fixmyride/repo/ServiceRequestRepo.java
+++
@@ -1,7 +0,0 @@
-package com.chaitu.fixmyride.repo;
-
-import com.chaitu.fixmyride.model.ServiceRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRequestRepo extends JpaRepository<ServiceRequest,Integer> {
-}
